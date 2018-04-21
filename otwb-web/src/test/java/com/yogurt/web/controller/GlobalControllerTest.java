661622f0ea2394a08222d46b0ea3a6a196631a00
yogurt_lei <yogurt_lei@foxmail.com>
2018-04-21 23:46:17 +0800

    optimize code

--- otwb-web/src/test/java/com/yogurt/web/controller/GlobalControllerTest.java
+++ otwb-web/src/test/java/com/yogurt/web/controller/GlobalControllerTest.java
@@ -1,19 +1,17 @@
 package com.yogurt.web.controller;
 
+import com.yogurt.web.controller.global.GlobalController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
